ace540ac3f61e681d60bb93be572e66d57e554b9
Lannister-Xiaolin <119xiaolin@163.com>
2020-06-19 22:32:26 +0800

    批量处理多目标合成

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="xl_tool", # Replace with your own username
-    version="0.1.6",
+    version="0.1.7",
     author="Xiaolin",
     author_email="119xiaolin@163.com",
     description="my tool function",
